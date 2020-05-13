d76fff0c042dfdbd7da6dfcc086f077453484df9
aliciacisnerosm <aliciacisnerosm@gmail.com>
2020-05-13 02:05:16 -0500

    add products model & route

---
+++ models/productModel.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+});
+
+const productsCollection = mongoose.model('products', productSchema);
+
+const Products = {
+  createProduct: function (newProduct) {
+    return productsCollection
+      .create(newProduct)
+      .then((createdProduct) => {
+        console.log('si entra');
+        return createdProduct;
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  },
+  getAllProducts: function () {
+    return productsCollection
+      .find()
+      .then((allProducts) => {
+        return allProducts;
+      })
+      .catch((err) => {
+        return err;
+      });
+  },
+  getProductbyName: function (name) {
+    return productsCollection
+      .find({ name: `${name}` })
+      .then((nameMatch) => {
+        return nameMatch;
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  },
+  deleteProduct: function (id) {
+    return productsCollection
+      .deleteOne({ id: id })
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        return err;
+      });
+  },
+  updateProduct: function (id, params) {
+    return productsCollection
+      .findOneAndUpdate({ id: id }, { $set: params }, { new: true })
+      .then((product) => {
+        return product;
+      })
+      .catch((err) => {
+        return err;
+      });
+  },
+};
+
+module.exports = { Products };
